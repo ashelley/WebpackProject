f94e8bc4dc8ec6501c8efe4022650c2afee1de87
ashelley <ashelley@gmail.com>
2017-01-04 19:05:46 -0800

    add all files

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,9 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: './src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ]
 };
